ad401edd66bd0398c9a6a75fc53ad581ebb82a74
DESKTOP-5DS23B3\wnsgu <wnsgud0750@gmail.com>
2017-09-27 19:18:15 +0900

    주소 수정

--- app/src/main/java/com/example/junhyeong/myapplication/Google/MapsActivity.java
+++ app/src/main/java/com/example/junhyeong/myapplication/Google/MapsActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.junhyeong.myapplication.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +22,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -21,7 +34,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String store_name;
     private Typeface BMJUA;
     private static final int MY_LOCATION_REQUEST_CODE = 1;
-
+    private String store_address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +48,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent intent = getIntent();
         store_name = intent.getStringExtra("store_name");
         String store_grade = intent.getStringExtra("store_grade");
-        String store_address = intent.getStringExtra("store_address");
+        //store_address = intent.getStringExtra("store_address");
         String store_call = intent.getStringExtra("store_call"); //전화번호아이콘 만들어지면 사용
 
         x = intent.getDoubleExtra("X", 0.0);
         y = intent.getDoubleExtra("Y", 0.0);
 
-        TextView tvStore_name, tvStore_address, tvStore_grade, title;
+        final TextView tvStore_name, tvStore_address, tvStore_grade, title;
         title = (TextView) findViewById(R.id.textView9);
         title.setTypeface(BMJUA);
 
@@ -50,19 +63,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         tvStore_grade = (TextView) findViewById(R.id.textView8);
 
         tvStore_name.setText(store_name);
-        tvStore_address.setText(store_address);
         tvStore_grade.setText(store_grade);
 
 
-        // 좌표계 (준희가 되면 삭제, 안되면 다시 해야함)
-
-        /*
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        StringRequest postStringRequest = new StringRequest(Request.Method.GET, "https://dapi.kakao.com/v2/local/geo/transcoord.json?x=" + x + "&y=" + y + "&input_coord=WTM&output_coord=WGS84", new Response.Listener<String>() {
+        JsonObjectRequest postStringRequest = new JsonObjectRequest(Request.Method.GET, "https://maps.googleapis.com/maps/api/geocode/json?language=ko&latlng=" +x+ "," +y+ "&key=AIzaSyCR6PUO1y9JYM6fPjk85fre94xNabcRqsA", new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(String response) {
-
-                Log.e("asfd","asdf" + response);
+            public void onResponse(JSONObject response) {
+                Log.e("response", "response :" + response.optJSONArray("results").optJSONObject(0).optString("formatted_address"));
+                store_address = response.optJSONArray("results").optJSONObject(0).optString("formatted_address").substring(5);
+                tvStore_address.setText(store_address);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -74,14 +84,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public Map getHeaders() throws AuthFailureError {
                 Map params = new HashMap();
-                params.put("client_id", "KakaoAk 19ecef4f68dc725c76b07d9302ef942a");
                 return params;
             }
         };
         requestQueue.add(postStringRequest);
-        */
-
-
     }
 
 
@@ -100,7 +106,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng location = new LatLng(x, y);
-        //mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.addMarker(new MarkerOptions().position(location).title(store_name));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 17.0f));
     }
