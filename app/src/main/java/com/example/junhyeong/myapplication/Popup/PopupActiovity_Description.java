8316e1fc51348b81651d9f47fbf5d5b26350d83c
baeyeonjin <hy10128@naver.com>
2017-10-16 17:41:53 +0900

    디자인 2차수정

--- app/src/main/java/com/example/junhyeong/myapplication/Popup/PopupActiovity_Description.java
+++ app/src/main/java/com/example/junhyeong/myapplication/Popup/PopupActiovity_Description.java
@@ -1,12 +1,9 @@
 package com.example.junhyeong.myapplication.Popup;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.example.junhyeong.myapplication.R;
@@ -22,10 +19,6 @@ public class PopupActiovity_Description extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_popup_copyright);
-        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        int width = (int) (display.getWidth() * 1.0);
-
-        getWindow().getAttributes().width = width;
 
         BtnClose = (ImageView)findViewById(R.id.copyright_close);
         BtnClose.setOnClickListener(new View.OnClickListener() {
